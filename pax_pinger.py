d3908bcdbfffc04b83ede7dc82269d698e5e475e
Michael Eaton <meaton@xby2.com>
2014-05-19 23:53:23 -0400

    Update pax_pinger.py
    Update to pull from configuration.xml

--- pax_pinger.py
+++ pax_pinger.py
@@ -1,31 +1,44 @@
 from twitter import *
+from configuration_reader import *
 import urllib
 import time
 import smtplib
 import re
 from email.mime.text import MIMEText
- 
-#Do Oauth Dance to get this rather than hardcode
-OAUTH_TOKEN="OAUTH_TOKEN";
-OAUTH_SECRET="OAUTH_SECRET";
- 
-CONSUMER_KEY="API_KEY";
-CONSUMER_SECRET="API_SECRET";
- 
-t_obj = Twitter( auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET,
-                              CONSUMER_KEY, CONSUMER_SECRET));
 
-#Hardcoded last tweet ID?  Hardcoded last tweet ID.
-#Whatever, it'll update itself if a new one pops up.
+#Set hardcoded parameters
+CONFIG_LOCATION = "configuration.xml";
+OAUTH_LOCATION = "oauth.dat";
+APP_NAME = "PAX_Pinger";
 Last_Tweet_ID = 467354116139020288;
 
-gmail_user = 'EMAIL'
-phone_number = 'PHONE_NUMBER'
-gmail_pwd = 'GMAIL_PASSWORD'
-recip2 = phone_number + 'PHONE_PROVIDER_TEXT_EMAIL'
-news = 'PAX Prime registration isn\'t yet available...follow our '
+#Get configuration
+config = getConfiguration(CONFIG_LOCATION);
+auth = config['auth'];
+contact = config['contact'];
+
+#Perform OAuth dance and store result if oauth keys not already present
+
+if(not auth['OAuthToken'] or not auth['OAuthSecret']):
+        oauth_dance(APP_NAME, auth['API_Key'], auth['API_Secret'],
+                    OAUTH_LOCATION);
+        auth['OAuthToken'], auth['OAuthSecret'] = read_token_file(OAUTH_LOCATION);
+        writeOAuthDanceValues(CONFIG_LOCATION, auth['OAuthToken'], auth['OAuthSecret']);
+        #Clean up after ourselves
+        os.remove(OAUTH_LOCATION);
+
+#Create our twitter object
+t_obj = Twitter( auth=OAuth(auth['OAuthToken'], auth['OAuthSecret'],
+                               auth['API_Key'], auth['API_Secret']));
+
+#Set values from config
+gmail_user = contact['User']
+phone_number = contact['PhoneNumber']
+gmail_pwd = contact['Password']
+recip2 = phone_number + contact['TextEmailServer']
+news = 'PAX Prime registration isn\'t yet available...follow our ' 
 soon = '<li class="soon"><h3>Soon</h3></li>'
- 
+
 def email(to, text):
     smtpserver = smtplib.SMTP("smtp.gmail.com",587)
     smtpserver.ehlo();
@@ -44,14 +57,14 @@ def email(to, text):
     except:
         print("ERROR, failed to email this cycle!");
         return -1;
- 
+
 def checkTweets(t_obj):
     try:
         tweets = t_obj.statuses.user_timeline(screen_name="Official_PAX");
     except:
         print("Tweet checking failed for some reason!");
         return -1;
-   
+    
     if(tweets[0]['id'] != Last_Tweet_ID):
         email(recip2, 'NEW TWEET FROM PAX!');
         print('New Tweet!');
@@ -59,12 +72,12 @@ def checkTweets(t_obj):
     else:
         print('No new Tweet');
         return -1;
- 
+
 while 1:
     try:
         aResp = urllib.request.urlopen('http://prime.paxsite.com/');
         web_pg = aResp.read().decode('utf-8');
- 
+
         newsmatches = re.findall(news, web_pg);
         soonmatches = re.findall(soon, web_pg);
         if len(newsmatches) == 0:
@@ -77,11 +90,11 @@ while 1:
             break;
         else:
             print("No Site changes");
- 
+
         tweetResult = checkTweets(t_obj);
         if(tweetResult != -1):
             Last_Tweet_ID = tweetResult;
     except:
         print("Shit fucked up, going back to bed.");
-       
+        
     time.sleep(60);
